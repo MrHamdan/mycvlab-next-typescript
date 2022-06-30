762812b270debbe703eab9b4e8595c073d15a5ee
Hamdan Ahmed <45794213+MrHamdan@users.noreply.github.com>
2022-06-30 17:31:21 +0600

    issue fixed

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,26 +1,28 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
-import Homepage from '../components/home/Homepage'
+import type { NextPage } from "next";
+import Head from "next/head";
+import Image from "next/image";
+import Homepage from "../components/home/Homepage";
 
-import styles from '../styles/Home.module.css'
+import styles from "../styles/Home.module.css";
+import StyledEngineProvider from "@mui/material/StyledEngineProvider";
 
 const Home: NextPage = () => {
   return (
     <div>
-      
-      <Head>
-        <link
-          rel="stylesheet"
-          href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css"
-          integrity="sha512-KfkfwYDsLkIlwQp6LFnl8zNdLGxu9YAA1QvwINks4PhcElQSvqcyVLLD9aMhXd13uQjoXtEKNosOWaZqXgel0g=="
-          crossOrigin="anonymous"
-          referrerPolicy="no-referrer"
-        />
-      </Head>
-      <Homepage />
+      <StyledEngineProvider injectFirst>
+        <Head>
+          <link
+            rel="stylesheet"
+            href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css"
+            integrity="sha512-KfkfwYDsLkIlwQp6LFnl8zNdLGxu9YAA1QvwINks4PhcElQSvqcyVLLD9aMhXd13uQjoXtEKNosOWaZqXgel0g=="
+            crossOrigin="anonymous"
+            referrerPolicy="no-referrer"
+          />
+        </Head>
+        <Homepage />
+      </StyledEngineProvider>
     </div>
   );
-}
+};
 
-export default Home
+export default Home;
