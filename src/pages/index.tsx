7ab56c4ee500f693506e6ca74b653e95286ac7dd
Hamdan Ahmed <45794213+MrHamdan@users.noreply.github.com>
2022-06-30 16:15:31 +0600

    adding file

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,8 +8,8 @@ import styles from '../styles/Home.module.css'
 const Home: NextPage = () => {
   return (
     <div>
+      
       <Head>
-        {/* Font Awesome Link */}
         <link
           rel="stylesheet"
           href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css"
